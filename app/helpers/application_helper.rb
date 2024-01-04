8dfb95e10fdae8a0f209fb3f556d1f1690f2fa8a
Jackson Stone <jackson@jstone.us>
2024-01-04 21:41:27 +0800

    Initialize Rails project (#1)
    * create rails project with 'rails new'
    
    * create rubocop action
    
    * change rubocop action
    
    * use bundler cache
    
    * add new gem
    
    * fix action to fail if rubocop finds issues
    
    * dont use code scanning
    
    * format for gh actions
    
    * try to fix gem issue
    
    * try to fix gem issue
    
    * try to disable caching?
    
    * should be finally fixed
    
    * fix rubocop issues, should pass
    
    * get rid of rubocop warning about new cops
    
    * add rubycritic ci
    
    * change rubycritic trigger
    
    * try and fix rubycritic action
    
    * add comment to rubycritic action
    
    * add comment to rubycritic action
    
    * automatically delete old comments
    
    * remove direct link to artifact
    
    * change verbiage in rubycritic comment

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+# Application Helper
+module ApplicationHelper
+end
---
+++ app/models/application_record.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+# Application Record
+class ApplicationRecord < ActiveRecord::Base
+  primary_abstract_class
+end
