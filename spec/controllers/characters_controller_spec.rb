b1cf2f4c47096e2a235c9767296a42a7bcc71a47
Jackson Stone <20364733+jacksors@users.noreply.github.com>
2024-01-10 11:03:48 +0800

    rubocop characters

--- spec/controllers/characters_controller_spec.rb
+++ spec/controllers/characters_controller_spec.rb
@@ -4,23 +4,23 @@ require 'rails_helper'
 
 RSpec.describe CharactersController, type: :controller do
   describe 'Admin access' do
-    let(:valid_attributes) {
+    let(:valid_attributes) do
       # Define valid attributes for your character model
       { name: 'Test Character', occupation: 'Test Occupation', inventory_slots: 10, balance: 100, type: 'Character' }
-    }
+    end
 
-    describe "GET #index" do
-      it "returns a success response" do
+    describe 'GET #index' do
+      it 'returns a success response' do
         Character.create! valid_attributes
         get :index, params: {}
         expect(response).to be_successful
       end
     end
 
-    describe "GET #show" do
-      it "returns a success response" do
+    describe 'GET #show' do
+      it 'returns a success response' do
         character = Character.create! valid_attributes
-        get :show, params: {id: character.to_param}
+        get :show, params: { id: character.to_param }
         expect(response).to be_successful
       end
     end
