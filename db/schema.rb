d924c93d02a772138d3645a475c86d91209d85ae
Jackson Stone <20364733+jacksors@users.noreply.github.com>
2024-01-10 10:00:38 +0800

    reorder migrations and update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_09_012904) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_09_002953) do
   create_table "characters", force: :cascade do |t|
     t.string "name"
     t.string "occupation"
