ed3204e1690c8c50aa439fbffb2fc9b8b6454e50
Jackson Stone <20364733+jacksors@users.noreply.github.com>
2024-01-10 10:55:09 +0800

    characters rspec

--- spec/controllers/characters_controller_spec.rb
+++ spec/controllers/characters_controller_spec.rb
@@ -3,7 +3,26 @@
 require 'rails_helper'
 
 RSpec.describe CharactersController, type: :controller do
-  describe 'admin actions' do
-    # TODO
+  describe 'Admin access' do
+    let(:valid_attributes) {
+      # Define valid attributes for your character model
+      { name: 'Test Character', occupation: 'Test Occupation', inventory_slots: 10, balance: 100, type: 'Character' }
+    }
+
+    describe "GET #index" do
+      it "returns a success response" do
+        Character.create! valid_attributes
+        get :index, params: {}
+        expect(response).to be_successful
+      end
+    end
+
+    describe "GET #show" do
+      it "returns a success response" do
+        character = Character.create! valid_attributes
+        get :show, params: {id: character.to_param}
+        expect(response).to be_successful
+      end
+    end
   end
 end
