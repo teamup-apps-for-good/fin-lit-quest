b2a9d1c85bf82b9f25db8aa0ea1a5c9af2c395b4
Justin <78627687+jamlol@users.noreply.github.com>
2024-01-12 00:47:41 +0800

    Add Basic Styling (#30)
    * added basic styling
    
    * added basic styling
    
    * rebased from main, made font changes

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ module FinLitQuest
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
-
+    config.assets.enabled = true
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
