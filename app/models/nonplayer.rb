0c49cdc30f089293c073892db14108ccb0362cb7
Justin Abraham <justin.a@tamu.edu>
2024-01-11 09:40:32 +0800

    fixed rubocop error

--- app/models/nonplayer.rb
+++ app/models/nonplayer.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# Nonplayer Model
 class Nonplayer < Character
   belongs_to :item_to_accept, class_name: 'Item', foreign_key: 'item_to_accept_id'
   belongs_to :item_to_offer, class_name: 'Item', foreign_key: 'item_to_offer_id'
