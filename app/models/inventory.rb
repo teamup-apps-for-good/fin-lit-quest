3ae76cd2087475fb9570da0fbf2d06a76cb8fd62
Stella Yang <stella@stellayang.dev>
2024-01-11 09:32:57 +0800

    sort out model relationships
    Co-Authored-By: Jackson Stone <20364733+jacksors@users.noreply.github.com>

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -2,6 +2,6 @@
 
 # Model for the Inventory table.
 class Inventory < ApplicationRecord
-  belongs_to :owner, class_name: 'Character', foreign_key: 'owner_id'
+  belongs_to :character
   belongs_to :item
 end
--- db/migrate/20240108032902_add_nonplayers_subclass.rb
+++ db/migrate/20240108032902_add_nonplayers_subclass.rb
@@ -2,9 +2,12 @@ class AddNonplayersSubclass < ActiveRecord::Migration[7.1]
   def change
     add_column :characters, :personality, :string
     add_column :characters, :dialogue_content, :text
-    add_column :characters, :item_to_accept, :integer
     add_column :characters, :quantity_to_accept, :integer
-    add_column :characters, :item_to_offer, :integer
     add_column :characters, :quantity_to_offer, :integer
+
+    change_table :characters do |t|
+      t.references :item_to_accept, null: true, foreign_key: { to_table: :items }
+      t.references :item_to_offer, null: true, foreign_key: { to_table: :items }
+    end
   end
 end
