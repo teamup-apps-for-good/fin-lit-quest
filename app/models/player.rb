11dc2c3baa56249137d44afa44528a83c8598259
KiranWells <kiranwells1008@gmail.com>
2024-01-26 17:42:10 -0600

    Change >= to > for player level

--- app/models/player.rb
+++ app/models/player.rb
@@ -3,5 +3,5 @@
 # player character model
 class Player < Character
   validates :name, :occupation, :inventory_slots, :balance, presence: true
-  validates :current_level, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :current_level, presence: true, numericality: { greater_than: 0 }
 end
