fa5dfbf91dc022abb149a384420607f010fe87ab
Nick Anthony <123604923+naam1595am@users.noreply.github.com>
2024-01-27 08:08:30 +0800

    Added character default image, added rspec tests for gameplays_helper… (#56)
    * Added character default image, added rspec tests for gameplays_helper.rb and styled town in town.css
    
    * Deleted Wrong File that does not belong to this chore

--- app/helpers/gameplays_helper.rb
+++ app/helpers/gameplays_helper.rb
@@ -2,4 +2,24 @@
 
 # GameplaysHelper
 module GameplaysHelper
+  def nonplayer_gif_exists?(nonplayer)
+    path = "#{nonplayer.name.downcase}.gif"
+    if Rails.application.assets
+      !Rails.application.assets.find_asset(path).nil?
+    else
+      !Rails.application.assets_manifest.find_sources(path).nil?
+    end
+  end
+
+  def nonplayer_gif_file(nonplayer)
+    if nonplayer_gif_exists?(nonplayer)
+      "#{nonplayer.name.downcase}.gif"
+    else
+      'generic_nonplayer.gif'
+    end
+  end
+
+  def nonplayer_img_tag(nonplayer)
+    "#{nonplayer.name.downcase}.gif"
+  end
 end
