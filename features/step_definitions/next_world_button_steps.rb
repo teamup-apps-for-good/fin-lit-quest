42a37a69a104881807c2e47f6275c8b5db303484
Emmie Teng <mengting.teng@gmail.com>
2024-01-28 01:32:01 -0600

    added cucumber step definition

---
+++ features/step_definitions/next_world_button_steps.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+When('{string} is given {string} {string}') do |character_name, amount, item_name|
+    Inventory.create(item: Item.find_by(name: item_name),
+                     character: Character.find_by(name: character_name),
+                     quantity: amount)    
+end
+  
