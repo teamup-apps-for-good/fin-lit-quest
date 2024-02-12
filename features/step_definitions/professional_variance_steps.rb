9464dcfa3836931eb26e8bb5a6de4fd9137bfea6
Emmie Teng <mengting.teng@gmail.com>
2024-02-12 14:53:58 -0600

    created template for professional_variance cucumber

---
+++ features/step_definitions/professional_variance_steps.rb
@@ -0,0 +1,4 @@
+Given('the following preference table exists:') do |preferences|
+    # table is a Cucumber::MultilineArgument::DataTable
+    pending # Write code here that turns the phrase above into concrete actions
+end
