735833eb983a1d84aca5de3cd92896a57733883e
naam1595am <naam1595@tamu.edu>
2024-02-14 19:20:33 -0600

    Updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_12_191545) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_15_011029) do
   create_table "characters", force: :cascade do |t|
     t.string "name"
     t.string "occupation"
