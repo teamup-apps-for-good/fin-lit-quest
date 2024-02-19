efabd2581cd317ce7f9624bff56326702af18b47
Jackson Stone <jackson.stone@frogslayer.com>
2024-02-19 15:49:12 -0600

    More coverage

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -4,16 +4,11 @@ require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
   before do
-    Player.create!(name: 'Stella', occupation: :farmer, inventory_slots: 5, balance: 0, current_level: 1,
-                   email: 'test@test.com', provider: 'google-oauth2', uid: '1234')
+    @player = Player.create!(name: 'Stella', occupation: :farmer, inventory_slots: 5, balance: 0, current_level: 1,
+                   email: 'test@test.com', provider: 'google_oauth2', uid: '1234')
   end
 
   describe 'GET #logged_in' do
-    before do
-      @player = Player.create!(name: 'Stella', occupation: :farmer, inventory_slots: 5, balance: 0, current_level: 1,
-                               email: 'test@test.com', provider: 'google-oauth2', uid: '1234')
-    end
-
     context 'when user is logged in' do
       it 'returns the current character' do
         session[:user_id] = @player.id
@@ -64,6 +59,76 @@ RSpec.describe SessionsController, type: :controller do
 
         expect(response).not_to be_redirect
       end
+    end
+  end
+
+  describe "GET #logout" do
+    context "when the user is logged in" do
+      it 'logs out the user' do
+        get :logout
+        expect(session[:user_id]).to be_nil
+      end
+
+      it 'redirects them to the welcome page' do
+        get :logout
+        expect(response).to redirect_to(welcome_path)
+      end
+    end
+  end
+
+  describe "GET #omniauth" do
+    before do
+      OmniAuth.config.test_mode = true
+      OmniAuth.config.add_mock(:google_oauth2, {
+        uid: '1234',
+        info: {
+          name: 'Stella',
+          email: 'test@test.com'
+        }
+      })
+      OmniAuth.config.add_mock(:github, {
+        uid: '1234',
+        info: {
+          name: 'Stella',
+          email: 'test@test.com'
+        }
+      })
+    end
+    context 'when the user is logged out' do
+      before do
+        session[:user_id] = nil
+      end
+
+      context 'if they have logged in before' do
+        before do
+          request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:google_oauth2]
+        end
+
+        it 'assigns the user id to the session' do
+          get :omniauth
+          expect(session[:user_id]).to eq(@player.id)
+        end
+
+        it 'redirects them to the root path' do
+          get :omniauth
+          expect(response).to redirect_to(root_path)
+        end
+      end
+
+      context 'if they have not logged in before' do
+        before do
+          request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:github]
+        end
+        it 'creates the new user' do
+          get :omniauth
+          new_player = Player.find_by(provider: :github, uid: '1234')
+          expect(new_player).to eq(Player.find(session[:user_id]))
+        end
+      end
+    end
+
+    after do
+      OmniAuth.config.test_mode = false
     end
   end
 end
