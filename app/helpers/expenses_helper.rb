e8119be501ea0d7ed5a16fcf46eab4e53b2f5774
Stella Yang <stella@stellayang.dev>
2024-02-21 00:59:40 -0600

    reduce complexity in display_expense_for char function

--- app/helpers/expenses_helper.rb
+++ app/helpers/expenses_helper.rb
@@ -2,17 +2,25 @@
 
 # Rendering the items in the expenses
 module ExpensesHelper
-  def self.display_expense_for_character(character)
+  def self.display_today_expense_for_character(character)
     return 'Character details incomplete or not found.' unless character.present?
 
     today_expense = Expense.today_expense(character.day)
-    weekly_expense = Expense.this_week_expense(character.day)
-    output = ''
-    output += "Expenses for today: #{today_expense.quantity} #{today_expense.item.name}" unless today_expense.nil?
-    unless weekly_expense.nil?
-      output += "\nExpenses for this week: #{weekly_expense.quantity} #{weekly_expense.item.name}"
+    if today_expense
+      "Expenses for today: #{today_expense.quantity} #{today_expense.item.name}"
+    else
+      ''
     end
+  end
 
-    output
+  def self.display_this_week_expense_for_character(character)
+    return 'Character details incomplete or not found.' unless character.present?
+
+    weekly_expense = Expense.this_week_expense(character.day)
+    if weekly_expense
+      "Expenses for this week: #{weekly_expense.quantity} #{weekly_expense.item.name}"
+    else
+      ''
+    end
   end
 end
