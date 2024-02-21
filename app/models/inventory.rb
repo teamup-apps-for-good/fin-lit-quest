000c5f17d1aed08243ff3fe040502728576bf119
Stella Yang <stella@stellayang.dev>
2024-02-21 00:49:34 -0600

    migrated expense checking and deduction into inventory

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -8,7 +8,40 @@ class Inventory < ApplicationRecord
   validates :character, presence: true
   validates :quantity, presence: true, numericality: { greater_than_or_equal_to: 0 }
 
-  def self.deduct_expense(character, expense)
-    Inventory.find_by(character:, item: expense.item).decrement!(:quantity, expense.quantity)
+  def self.deduct_expense(player, expense)
+    Inventory.find_by(character: player, item: expense.item).decrement!(:quantity, expense.quantity)
   end
+
+  def self.satisfy_expense?(player, expense1, expense2)
+    return true if expense1.nil? && expense2.nil?
+    return satisfy_single_expense?(player, expense1) if expense2.nil?
+    return satisfy_single_expense?(player, expense2) if expense1.nil?
+    return satisfy_same_expense?(player, expense1, expense2) if expense1.item == expense2.item
+
+    satisfy_diff_expense?(player, expense1, expense2)
+  end
+
+  def self.satisfy_single_expense?(player, expense)
+    inventory = Inventory.find_by(character: player, item: expense.item)
+    inventory && inventory.quantity >= expense.quantity
+  end
+
+  def self.satisfy_same_expense?(player, expense1, expense2)
+    inventory = Inventory.find_by(character: player, item: expense1.item)
+    inventory && inventory.quantity >= expense1.quantity + expense2.quantity
+  end
+
+  def self.satisfy_diff_expense?(player, expense1, expense2)
+    inventory1 = Inventory.find_by(character: player, item: expense1.item)
+    inventory2 = Inventory.find_by(character: player, item: expense2.item)
+
+    return false unless inventory1 && inventory2
+
+    enough_quantity1 = inventory1.quantity >= expense1.quantity
+    enough_quantity2 = inventory2.quantity >= expense2.quantity
+
+    enough_quantity1 && enough_quantity2
+  end
+
+  private_class_method :satisfy_single_expense?, :satisfy_same_expense?, :satisfy_diff_expense?
 end
