c1333697fdb56b2b7cafc04b72c0ed6054f106e1
Jackson Stone <jackson.stone@frogslayer.com>
2024-02-22 13:13:15 -0600

    Run rubocop

--- features/step_definitions/feature-specific/track_player_steps.rb
+++ features/step_definitions/feature-specific/track_player_steps.rb
@@ -1,3 +1,5 @@
-Then('I should be on the inventory page for {string}') do |string|
+# frozen_string_literal: true
+
+Then('I should be on the inventory page for {string}') do |_string|
   pending # Write code here that turns the phrase above into concrete actions
 end
