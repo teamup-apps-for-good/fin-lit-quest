49256316bc3328520339f0bc4ebd3f8d6b7c658d
victorpan04 <victorp168@live.com>
2024-03-19 14:10:44 -0500

    passes all cucumber tests except for first login

--- app/controllers/tutorials_controller.rb
+++ app/controllers/tutorials_controller.rb
@@ -2,5 +2,12 @@ class TutorialsController < ApplicationController
   def show
     @tutorial_level = @current_user.current_level
     @page = params[:page].present? ? params[:page].to_i : 1
+    if @tutorial_level == 1
+      @last_page = 6
+    elsif @tutorial_level == 2
+      @last_page = 7
+    else
+      @last_page = 1
+    end
   end
 end
