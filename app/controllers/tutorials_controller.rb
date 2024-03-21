2c9185dd0d8d2d479625466f95d2c5bea64997cc
victorpan04 <victorp168@live.com>
2024-03-21 00:38:16 -0500

    added more rspec for tutorial

--- app/controllers/tutorials_controller.rb
+++ app/controllers/tutorials_controller.rb
@@ -5,12 +5,7 @@ class TutorialsController < ApplicationController
   def show
     @tutorial_level = @current_user.current_level
     @page = params[:page].present? ? params[:page].to_i : 1
-    @last_page = if @tutorial_level == 1
-                   6
-                 elsif @tutorial_level == 2
-                   7
-                 else
-                   1
-                 end
+    last_page_for_level = { 1 => 6, 2 => 7 }
+    @last_page = last_page_for_level[@tutorial_level] || 1
   end
 end
