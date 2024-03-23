cebf3ac11fb45e494a1bbb116f140e3d4405a534
victorpan04 <victorp168@live.com>
2024-03-22 19:51:11 -0500

    tutorial now has files per level, yml file for level/pages for maintainability

--- app/controllers/tutorials_controller.rb
+++ app/controllers/tutorials_controller.rb
@@ -1,11 +1,19 @@
-# frozen_string_literal: true
-
-# TutorialsContrller
 class TutorialsController < ApplicationController
+  rescue_from ActionView::MissingTemplate, with: :redirect_to_construction
+
   def show
     @tutorial_level = @current_user.current_level
     @page = params[:page].present? ? params[:page].to_i : 1
-    last_page_for_level = { 1 => 6, 2 => 7 }
-    @last_page = last_page_for_level[@tutorial_level] || 1
+    tutorial_levels = YAML.load_file("#{Rails.root}/config/tutorial_levels.yml")
+    @last_page = tutorial_levels[@tutorial_level]&.to_i || 1
+    render "tutorials/era_#{@tutorial_level}"
+  end
+  
+
+  private
+
+  def redirect_to_construction
+    @tutorial_level = @current_user.current_level
+    render "tutorials/era_construction"
   end
 end
