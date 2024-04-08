ac550d4233c1306f7732d09abef554da35fd7692
Kiran Wells <64935184+KiranWells@users.noreply.github.com>
2024-04-08 12:22:24 -0500

    Game over page improvements (#113)

---
+++ features/game_over_improvements.feature
@@ -0,0 +1,39 @@
+Feature: Gameover Page
+
+As a player
+so that I can recover from a game over state
+I want to the game over page to have good styling and allow me to go back to the start with new starter items
+
+  Background:
+    Given the following players exist:
+      | name       | occupation | inventory_slots | balance | current_level | uid  | provider      | email         |
+      | Player     | programmer | 5               | 0       | 0             | 5678 | google_oauth2 | test@test.com |
+    Given the following items exist:
+      | name   | description                                        | value |
+      | wheat  | grainy, fresh from the field                       | 1     |
+      | boots  | sturdy and waterproof, fresh from the cobbler      | 25    |
+      | grapes | -                                                  | 2     |
+    Given the following starter item table exists:
+      | item   | quantity |
+      | wheat  | 10       |
+      | boots  | 1        |
+      | grapes | 3        |
+
+  Scenario: I can see the game over button
+    Given I am logged in as "Player"
+    And I am on the game over page
+    Then I should see "Restart"
+
+  Scenario: I can click the restart button and restart game
+    Given I am logged in as "Player"
+    And I am on the game over page
+    When I click on "Restart"
+    Then I should be on the home page
+    And I should see "Game restarted successfully."
+
+Scenario: After restarting my inventory should not be empty
+    Given I am logged in as "Player"
+    And I am on the game over page
+    When I click on "Restart"
+    Then I should be on the home page
+    And "Player"'s inventory should not be empty
--- spec/controllers/gameplays_controller_spec.rb
+++ spec/controllers/gameplays_controller_spec.rb
@@ -48,6 +48,11 @@ RSpec.describe GameplaysController, type: :controller do
       get :town
       expect(response).to redirect_to(game_over_path)
     end
+    it 'should redirect to home if current_level is not zero' do
+      session[:user_id] = @current_user.id
+      get :game_over
+      expect(response).to redirect_to(root_path)
+    end
   end
 
   describe 'restart' do
@@ -58,7 +63,48 @@ RSpec.describe GameplaysController, type: :controller do
       expect(@current_user.balance).to eq(0)
       expect(@current_user.day).to eq(1)
       expect(@current_user.hour).to eq(1)
+
       expect(response).to redirect_to(root_path)
+    end
+  end
+
+  context 'starter items' do
+    before do
+      item1 = Item.create!(name: 'test item', description: 'test', value: 1)
+      item2 = Item.create!(name: 'test item 2', description: 'test', value: 1)
+      @item3 = Item.create!(name: 'test item 3', description: 'test', value: 1)
+
+      @si1 = StarterItem.create!({ item: item1, quantity: 1 })
+      @si2 = StarterItem.create!({ item: item2, quantity: 2 })
+    end
+
+    after do
+      StarterItem.destroy_all
+      Inventory.destroy_all
+      Item.destroy_all
+    end
+
+    describe 'adding starter items' do
+      it 'adds all starting items to a player when they restart' do
+        get :restart
+        @current_user.reload
+
+        inv1 = Inventory.find_by(item: @si1.item, character: @current_user)
+        expect(inv1).not_to be_nil
+        expect(inv1.quantity).to eq(1)
+        inv2 = Inventory.find_by(item: @si2.item, character: @current_user)
+        expect(inv2).not_to be_nil
+        expect(inv2.quantity).to eq(2)
+      end
+
+      it 'removes all previous items when restarting' do
+        Inventory.create!(item: @item3, character: @current_user, quantity: 4)
+        get :restart
+        @current_user.reload
+
+        inv3 = Inventory.find_by(item: @item3, character: @current_user)
+        expect(inv3).to be_nil
+      end
     end
   end
 end
